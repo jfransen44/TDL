69d21474bf68b5615dd1299d174e3f5ced4b8fdc
JFransen44 <jfransen@csumb.edu>
2015-12-14 17:53:04 -0800

    added dialog boxes

--- app/src/main/java/com/example/jfransen44/tdl/MainActivity.java
+++ app/src/main/java/com/example/jfransen44/tdl/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.jfransen44.tdl;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import com.firebase.client.ChildEventListener;
@@ -22,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     private String taskString = "";
     private String taskDate = "";
     private String taskTime = "";
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private Button clearButton;
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get widgets from xml
-        final ListView listView = (ListView) findViewById(R.id.listView);
-        final EditText text = (EditText) findViewById(R.id.todoText);
-        final Button button = (Button) findViewById(R.id.addButton);
+        listView = (ListView) findViewById(R.id.listView);
+        //final EditText text = (EditText) findViewById(R.id.todoText);
+        addButton = (Button) findViewById(R.id.addButton);
+        clearButton = (Button) findViewById(R.id.clearButton);
 
         // Create a new Adapter
-        final ArrayAdapter adapter = new ArrayAdapter<>(this,
-           android.R.layout.simple_list_item_multiple_choice, android.R.id.text1);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1);
 
         // Assign adapter to ListView
         listView.setAdapter(adapter);
@@ -44,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         // Use Firebase to populate the list.
         Firebase.setAndroidContext(this);
 
-        new Firebase("https://YOUR-FIREBASE-APP.firebaseio.com/todoItems")
+        new Firebase("https://glowing-torch-1077.firebaseio.com/todoItems")
            .addChildEventListener(new ChildEventListener() {
                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                    adapter.add((String) dataSnapshot.child("text").getValue());
@@ -66,18 +73,113 @@ public class MainActivity extends AppCompatActivity {
 
         // Add items via the Button and EditText at the bottom of the window.
 
-        button.setOnClickListener(new View.OnClickListener() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 getTask(v);
             }
         });
+        setUpClearButtonListener();
+        setupLongClickListener();
 
-        // Delete items when clicked
+        //change item background color if checkbox is selected
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("TEST", Integer.toString(position));
+                if (listView.isItemChecked(position))
+                    view.setBackgroundColor(Color.GRAY);
+                else
+                    view.setBackgroundColor(Color.TRANSPARENT);
+            }
+        });
 
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                new Firebase("https://YOUR-FIREBASE-APP.firebaseio.com/todoItems")
+        //Log.d("TEST", listView.getItemAtPosition(1).toString());
+        //VerifyData v = new VerifyData(adapter);
+    }//end onCreate
+
+    //called when user clicks add task button; start new activity
+    public void getTask(View view){
+        Intent getTaskIntent = new Intent(this, AddTaskActivity.class);
+        startActivityForResult(getTaskIntent, ENTER_TASK_REQUEST);
+    }//end getTask
+
+    //get results from AddTaskActivity
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ENTER_TASK_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                this.taskString = data.getStringExtra("taskString");
+                this.taskDate = data.getStringExtra("dateString");
+                this.taskTime = data.getStringExtra("timeString");
+                new Firebase("https://glowing-torch-1077.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(taskString + " " + taskDate + " " + " " + taskTime);
+            }
+
+        }
+    }//end onActivityResult
+
+    //set long click listener
+    public void setupLongClickListener(){
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int position, long id) {
+                makeDialogBox(position, item);
+                item.setBackgroundColor(Color.CYAN);
+                return true;
+            }
+        });
+    }//end setupLongClickListener
+
+    //delete selected items
+    private void setUpClearButtonListener(){
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i = 0; i < adapter.getCount(); i++){
+                    if (listView.isItemChecked(i)) {
+                        new Firebase("https://glowing-torch-1077.firebaseio.com/todoItems")
+                                .orderByChild("text")
+                                .equalTo((String) listView.getItemAtPosition(i))
+                                .addListenerForSingleValueEvent(new ValueEventListener() {
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.hasChildren()) {
+                                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                            firstChild.getRef().removeValue();
+                                        }
+                                    }
+
+                                    public void onCancelled(FirebaseError firebaseError) {
+                                    }
+                                });
+                        listView.setItemChecked(i, false);
+                    }
+                }
+            }
+        });
+    }//end setUpClearButtonListener
+
+    //Create dialog boxes
+
+    //Create dialog box with buttons for delete task, send task to email, and cancel
+    //accepts int position as reference to the location in adapter of selected item
+    private void makeDialogBox(final int position, View item) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View newItem = item;
+        builder.setMessage(R.string.builderMessage);
+        builder.setCancelable(false);
+        builder.setNegativeButton(R.string.negButtonMessage, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                newItem.setBackgroundColor(Color.TRANSPARENT);
+            }
+        });
+
+        //set up button and listener for delete
+        builder.setPositiveButton(R.string.posButtonMessage, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                new Firebase("https://glowing-torch-1077.firebaseio.com/todoItems")
                         .orderByChild("text")
                         .equalTo((String) listView.getItemAtPosition(position))
                         .addListenerForSingleValueEvent(new ValueEventListener() {
@@ -93,26 +195,27 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         });
-    }
 
-    //called when user clicks add task button
-    public void getTask(View view){
-        Intent getTaskIntent = new Intent(this, AddTaskActivity.class);
-        startActivityForResult(getTaskIntent, ENTER_TASK_REQUEST);
-    }
 
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == ENTER_TASK_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                this.taskString = data.getStringExtra("taskString");
-                this.taskDate = data.getStringExtra("dateString");
-                this.taskTime = data.getStringExtra("timeString");
-                new Firebase("https://YOUR-FIREBASE-APP.firebaseio.com/todoItems")
-                        .push()
-                        .child("text")
-                        .setValue(taskString + " " + taskDate + " " + " " + taskTime);
+        //set up button and listener for send task to email
+        builder.setNeutralButton(R.string.create_mail, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent createEmail = new Intent(Intent.ACTION_SEND);
+                createEmail.setType("plain/text");
+                createEmail.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+                createEmail.putExtra(Intent.EXTRA_TEXT, adapter.getItem(position).toString());
+                startActivity(Intent.createChooser(createEmail, "Send mail"));
             }
+        });
 
-        }
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        Button posButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        Button negButton = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        posButton.setBackgroundColor(Color.RED);
+        posButton.setTextColor(Color.BLACK);
+        negButton.setTextColor(Color.BLACK);
     }
 }
+
